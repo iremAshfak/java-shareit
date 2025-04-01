c996e10562616dc7f361f622f2db323a966efc9f
Irem <irem.ashfak@gmail.com>
2025-04-01 22:39:52 +0300

    commit

--- src/main/java/ru/practicum/shareit/comment/service/CommentService.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.comment.service;
-
-import ru.practicum.shareit.comment.dto.CommentDto;
-import ru.practicum.shareit.comment.dto.CommentResponseDto;
-
-public interface CommentService {
-    CommentResponseDto addComment(CommentDto commentDto, long itemId, long userId);
-}
